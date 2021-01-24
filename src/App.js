091960756259d372d9412807cdd9a1d26c64774f
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2021-01-24 18:09:12 +0530

    basic component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>App</h1>
+    <div>
+      <h1>Todos</h1>
     </div>
   )
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Todos extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Todos</h1>
+      </div>
+    )
+  }
+}
+
+export default Todos
