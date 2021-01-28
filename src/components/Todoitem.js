8feaa6af212b764c211653317288cecc03632e53
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2021-01-28 23:34:03 +0530

    add some styling

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -3,21 +3,25 @@ import PropTypes from 'prop-types'
 
 export class Todoitem extends Component {
   getStyle = () => {
-    if (this.props.todo.completed) {
-      return {
-        textDecoration: 'line-through',
-      }
-    } else {
-      return {
-        textDecoration: 'none',
-      }
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none',
     }
+  }
+
+  markComplete = (e) => {
+    console.log(this.props)
   }
 
   render() {
     return (
       <div style={this.getStyle()}>
-        <p>{this.props.todo.title}</p>
+        <p>
+          <input type='checkbox' onChange={this.props.markComplete} />{' '}
+          {this.props.todo.title}
+        </p>
       </div>
     )
   }
